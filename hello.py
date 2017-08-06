55052a0086f6e834567f81329ef053d6e8627d7d
shiguang328 <13052353610@163.com>
2017-08-06 11:54:00 +0800

    课本位置：P28， 修改user.html之前备份

--- hello.py
+++ hello.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template
 from flask import make_response
 from flask_bootstrap import Bootstrap
-
+from flask_script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer','42')
-    return response
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
+
 if __name__ =='__main__':
+    #manager.run()
     app.run(debug=True)
