433ba1a3080211bfc60d0582631be495b14216ce
shiguang328 <13052353610@163.com>
2018-10-11 18:23:08 +0800

    与书中版本10b同步

--- app/models.py
+++ app/models.py
@@ -1,15 +1,73 @@
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
+
+
+class Permission:
+    FOLLOW = 1
+    COMMENT = 2
+    WRITE = 4
+    MODERATE = 8
+    ADMIN = 16
 
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    @staticmethod
+    def insert_roles():
+        ''' insert_roles()函数不直接创建新角色对象，而是通过角色名查找现有的角色，然后再进行更新，只有
+        当数据库中没有某个角色时才创建。如此一来，以后更新了角色列表，就可以执行更新操作。想要新增、修改
+        角色，只需在roles数组中修改，然后执行该函数。
+            想要把角色写入数据库，可以使用shell回话：
+            (venv) $ python manage.py shell
+            >>> Role.insert_roles()
+            >>> Role.query.all()
+        '''
+        roles = {
+            'User': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE],
+            'Moderator': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE, Permission.MODERATE],
+            'Administrator': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE,
+                              Permission.MODERATE, Permission.ADMIN]
+        }
+        default_role = 'User'
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.reset_permissions()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permissions(self):
+        self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -23,6 +81,22 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)  # 邮箱是否确认
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        ''' 注册用户是赋予角色，首先判断是否为管理员（配置中的FLASKY_ADMIN保存的电子邮件识别），
+        只要这个邮箱在请求中出现，就赋予管理员角色，否则赋予默认角色。
+        '''
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(name='Administrator').first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()  # 设为默认角色
 
     @property
     def password(self):
@@ -35,13 +109,13 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    # 通过用户id生成一个令牌（一般用于发送邮箱确认链接），有效期默认为一个小时
     def generate_confirmation_token(self, expiration=3600):
+        ''' 通过用户id生成一个令牌（一般用于发送邮箱确认链接），有效期默认为一个小时 '''
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'confirm': self.id}).decode('utf-8')
 
-    # 校验令牌，通过则返回True，并把数据库中的confirmed字段设为True
     def confirm(self, token):
+        ''' 校验令牌，通过则返回True，并把数据库中的confirmed字段设为True '''
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
             data = s.loads(token.encode('utf-8'))
@@ -53,13 +127,78 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    def generate_reset_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'reset': self.id}).decode('utf-8')
+
+    @staticmethod
+    def reset_password(token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        user = User.query.get(data.get('reset'))
+        if user is None:
+            return False
+        user.password = new_password
+        db.session.add(user)
+        return True
+
+    def generate_email_change_token(self, new_email, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps(
+            {'change_email': self.id, 'new_email': new_email}).decode('utf-8')
+
+    def change_email(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('change_email') != self.id:
+            return False
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        self.email = new_email
+        db.session.add(self)
+        return True
+
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
+
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+
     def __repr__(self):
         return '<User %r>' % self.username
 
 
-# Falsk-Login要求程序实现一个回调函数，使用指定的标识符（特征）从数据库加载用户
-# 此处使用用户id加载
-# 如果找到用户，必须返回用户Object，否则返回None
+class AnonymousUser(AnonymousUserMixin):
+    ''' 该类继承自Flask-Login中的AnonymousUserMixin，并将其设为用户未登陆时的current_user值，
+    这样程序不用先检查用户是否登陆，就能够自由调用current_user.can()和current_user.is_administrator()
+    方法。
+    '''
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+login_manager.anonymous_user = AnonymousUser
+
+
 @login_manager.user_loader
 def load_user(user_id):
+    ''' Falsk-Login要求程序实现一个回调函数，使用指定的标识符（特征）从数据库加载用户
+    此处使用用户id加载
+    如果找到用户，必须返回用户Object，否则返回None
+    '''
     return User.query.get(int(user_id))
